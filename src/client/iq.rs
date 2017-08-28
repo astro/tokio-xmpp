874c3d97641b1b837829177bf3800df4d404533e
Astro <astro@spaceboyz.net>
2017-08-29 00:16:30 +0200

    iq: further work

--- src/client/iq.rs
+++ src/client/iq.rs
@@ -13,13 +13,36 @@ pub fn generate_id(len: usize) -> String {
     thread_rng().gen_ascii_chars().take(len).collect()
 }
 
+/// `(stanza.from, atanza.to)`
+type IqIdentification = (String, String);
+
+pub trait IdentifiesIq {
+    fn identify(self) -> Option<IqIdentification>;
+}
+
+/// For any `(String, String)`
+impl IdentifiesIq for IqIdentification {
+    fn identify(self) -> Option<IqIdentification> {
+        Some(self)
+    }
+}
+
+/// Extracts from stanza
+impl<'a> IdentifiesIq for &'a Element {
+    fn identify(self) -> Option<IqIdentification> {
+        self.attr("id").and_then(
+            |id| self.attr("from").map(
+                |from| (from.to_owned(), id.to_owned())
+            ))
+    }
+}
+
 /// An `FnOnce` callback would give more freedom to users Check back
 /// at a later Rust version.
 type Callback = FnMut(Result<&Element, &Element>) -> Box<Future<Item = (), Error = String>>;
 
-// TODO: add identification by (from, id)
 pub struct Tracker {
-    pending: HashMap<String, Box<Callback>>,
+    pending: HashMap<IqIdentification, Box<Callback>>,
 }
 
 impl Tracker {
@@ -29,15 +52,28 @@ impl Tracker {
         }
     }
 
-    pub fn has_id(&self, id: &str) -> bool {
-        self.pending.get(id).is_some()
+    pub fn has<K: IdentifiesIq>(&self, key: K) -> bool {
+        match key.identify() {
+            Some(ident) =>
+                self.pending.get(&ident).is_some(),
+            None =>
+                false,
+        }
     }
-    
-    pub fn insert(&mut self, id: String, callback: Box<Callback>) {
-        self.pending.insert(id, callback);
+
+    pub fn insert<K: IdentifiesIq>(&mut self, key: K, callback: Box<Callback>) {
+        match key.identify() {
+            Some(ident) => {
+                self.pending.insert(ident, callback);
+            },
+            None =>
+                (),
+        }
     }
 
     pub fn trigger(&mut self, stanza: &Element) -> Box<Future<Item = bool, Error = String>> {
+        let key = stanza.identify();
+
         let arg = if stanza.name() == "iq" && stanza.attr("type") == Some("result") {
             Some(Ok(stanza))
         } else if stanza.name() == "iq" && stanza.attr("type") == Some("error") {
@@ -47,13 +83,8 @@ impl Tracker {
         };
         arg.and_then(
             |arg| {
-                let get_id = |stanza: &Element| stanza.attr("id").map(|id| id.to_owned());
-                let id: Option<String> =
-                    arg
-                    .map(&get_id)
-                    .unwrap_or_else(&get_id);
-                id.and_then(
-                    |id| self.remove(&id).map(
+                key.and_then(
+                    |key| self.remove(key).map(
                         |mut callback: Box<Callback>| {
                             let result: Box<Future<Item = bool, Error = String>> =
                                 Box::new(
@@ -68,8 +99,10 @@ impl Tracker {
         )
     }
 
-    pub fn remove(&mut self, id: &str) -> Option<Box<Callback>> {
-        self.pending.remove(id)
+    pub fn remove<K: IdentifiesIq>(&mut self, key: K) -> Option<Box<Callback>> {
+        key.identify().and_then(
+            |ident| self.pending.remove(&ident)
+        )
     }
 }
 
@@ -77,36 +110,109 @@ impl Tracker {
 mod tests {
     use super::*;
     use minidom::Element;
+    use std::cell::RefCell;
+    use std::rc::Rc;
 
     #[test]
     fn test_has_id() {
         let mut tracker = Tracker::new();
-        tracker.insert("abc".to_owned(), Box::new(|_| Box::new(future::ok(()))));
-        assert!(tracker.has_id("abc"));
+        let ident = ("a@example.com".to_owned(), "abc".to_owned());
+        tracker.insert(ident.clone(), Box::new(|_| Box::new(future::ok(()))));
+        assert!(tracker.has(ident));
+    }
+
+    #[test]
+    fn test_remove_existing() {
+        let mut tracker = Tracker::new();
+        let ident = ("a@example.com".to_owned(), "abc".to_owned());
+        tracker.insert(ident.clone(), Box::new(|_| Box::new(future::ok(()))));
+        assert!(tracker.remove(ident).is_some());
+    }
+
+    #[test]
+    fn test_remove_missing() {
+        let mut tracker = Tracker::new();
+        let ident = ("a@example.com".to_owned(), "abc".to_owned());
+        tracker.insert(ident, Box::new(|_| Box::new(future::ok(()))));
+        let ident2 = ("b@example.com".to_owned(), "xyz".to_owned());
+        assert!(tracker.remove(ident2).is_none());
     }
 
     #[test]
     fn test_trigger_simple_result() {
         let mut tracker = Tracker::new();
-        tracker.insert("abc".to_owned(), Box::new(|_| Box::new(future::ok(()))));
+        let ident = ("a@example.com".to_owned(), "abc".to_owned());
+        let callback_called = Rc::new(RefCell::new(false));
+        let callback_called_ = callback_called.clone();
+        tracker.insert(ident, Box::new(move |iq_stanza| {
+            assert!(iq_stanza.is_ok());
+            *callback_called_.borrow_mut() = true;
+            Box::new(future::ok(()))
+        }));
         let iq = Element::builder("iq")
             .attr("from", "a@example.com")
             .attr("id", "abc")
             .attr("type", "result")
             .build();
         assert_eq!(tracker.trigger(&iq).wait(), Ok(true));
-        // TODO: verify result
+        assert_eq!(*callback_called.borrow(), true);
+    }
+
+    #[test]
+    fn test_trigger_simple_error() {
+        let mut tracker = Tracker::new();
+        let ident = ("a@example.com".to_owned(), "abc".to_owned());
+        let callback_called = Rc::new(RefCell::new(false));
+        let callback_called_ = callback_called.clone();
+        tracker.insert(ident, Box::new(move |iq_stanza| {
+            assert!(iq_stanza.is_err());
+            *callback_called_.borrow_mut() = true;
+            Box::new(future::ok(()))
+        }));
+        let iq = Element::builder("iq")
+            .attr("from", "a@example.com")
+            .attr("id", "abc")
+            .attr("type", "error")
+            .build();
+        assert_eq!(tracker.trigger(&iq).wait(), Ok(true));
+        assert_eq!(*callback_called.borrow(), true);
     }
 
     #[test]
     fn test_trigger_wrong_id() {
         let mut tracker = Tracker::new();
-        tracker.insert("abc".to_owned(), Box::new(|_| Box::new(future::ok(()))));
+        let ident = ("a@example.com".to_owned(), "abc".to_owned());
+        let callback_called = Rc::new(RefCell::new(false));
+        let callback_called_ = callback_called.clone();
+        tracker.insert(ident, Box::new(move |iq_stanza| {
+            *callback_called_.borrow_mut() = true;
+            Box::new(future::ok(()))
+        }));
         let iq = Element::builder("iq")
             .attr("from", "a@example.com")
             .attr("id", "xyz")
             .attr("type", "result")
             .build();
         assert_eq!(tracker.trigger(&iq).wait(), Ok(false));
+        assert_eq!(*callback_called.borrow(), false);
+    }
+
+    #[test]
+    fn test_trigger_wrong_from() {
+        let mut tracker = Tracker::new();
+        let ident = ("a@example.com".to_owned(), "abc".to_owned());
+        let callback_called = Rc::new(RefCell::new(false));
+        let callback_called_ = callback_called.clone();
+        tracker.insert(ident, Box::new(move |iq_stanza| {
+            *callback_called_.borrow_mut() = true;
+            Box::new(future::ok(()))
+        }));
+        let iq = Element::builder("iq")
+            .attr("from", "b@example.com")
+            .attr("id", "abc")
+            .attr("type", "result")
+            .build();
+        assert_eq!(tracker.trigger(&iq).wait(), Ok(false));
+        assert_eq!(*callback_called.borrow(), false);
     }
 }
